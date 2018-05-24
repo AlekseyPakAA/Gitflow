1b7ca9e90b0783710b53f9d0f9e9b8733683598d
aleksey_pak_aa <alexey.pak@umbrella-web.com>
2018-05-24 19:03:59 +0300

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gitflow
+//
+//  Created by Alexey Pak on 24/05/2018.
+//  Copyright © 2018 Alexey Pak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
